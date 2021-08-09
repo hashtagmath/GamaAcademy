42716ae525573a36d9f1c959c346db3bddf585ae
hashtagmath <matheussilvadossantos0@gmail.com>
2021-08-08 22:56:40 -0300

    js básico

---
+++ JsBasico/funcoes.js
@@ -0,0 +1,18 @@
+function soma(operadorA, operadorB) {
+    var resultadoC = operadorA + operadorB;
+    return resultadoC;
+}
+
+
+function olaGama(nome){
+    console.log("Olá Gama! você é o " + nome)
+}
+
+var resultadoDaSoma = soma(1,2);
+var resultadoNovoDaSoma = soma(10,66);
+
+console.log(resultadoDaSoma);
+console.log(resultadoNovoDaSoma);
+
+
+olaGama('Matheus');
